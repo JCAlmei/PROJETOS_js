11de0d22a6e4bd7253e3817de97a0234a9eca173
JeanCarlosAlmeida <jean.carlos.almeida1989@gmail.com>
2023-12-06 11:08:48 -0300

    Loops em JS - Desenv.Extra-2.3 #97293

---
+++ loops_des2.3.js
@@ -0,0 +1,49 @@
+numerosDaSorte = [37, 14, 26, 5, 94, 87]
+
+if (numerosDaSorte[0] < 50 && numerosDaSorte[0] % 0) {
+  console.log (numerosDaSorte[0],"é par e menor que 50")
+} else if (numerosDaSorte[0] > 50) {
+  console.log (numerosDaSorte[0],"é maior que 50")
+} else { 
+  console.log (numerosDaSorte[0],"é menor que 50")
+}
+
+if (numerosDaSorte[1] < 50 && numerosDaSorte[1] % 2 == 0) {
+  console.log (numerosDaSorte[1],"é par e menor que 50")
+} else if (numerosDaSorte[1] > 50) {
+  console.log (numerosDaSorte[1],"é maior que 50")
+} else { 
+  console.log (numerosDaSorte[1],"é menor que 50")
+}
+
+if (numerosDaSorte[2] < 50 && numerosDaSorte[2] % 2 == 0) {
+  console.log (numerosDaSorte[2],"é par e menor que 50")
+} else if (numerosDaSorte[2] > 50) {
+  console.log (numerosDaSorte[2],"é maior que 50")
+} else { 
+  console.log (numerosDaSorte[2],"é menor que 50")
+}
+
+if (numerosDaSorte[3] < 50 && numerosDaSorte[3] % 2 == 0) {
+  console.log (numerosDaSorte[3],"é par e menor que 50")
+} else if (numerosDaSorte[3] > 50) {
+  console.log (numerosDaSorte[3],"é maior que 50")
+} else { 
+  console.log (numerosDaSorte[3],"é menor que 50")
+}
+
+if (numerosDaSorte[4] < 50 && numerosDaSorte[4] % 2 == 0) {
+  console.log (numerosDaSorte[4],"é par e menor que 50")
+} else if (numerosDaSorte[4] > 50) {
+  console.log (numerosDaSorte[4],"é maior que 50")
+} else { 
+  console.log (numerosDaSorte[4],"é menor que 50")
+}
+
+if (numerosDaSorte[5] < 50 && numerosDaSorte[5] % 2 == 0) {
+  console.log (numerosDaSorte[5],"é par e menor que 50")
+} else if (numerosDaSorte[5] > 50) {
+  console.log (numerosDaSorte[5],"é maior que 50")
+} else { 
+  console.log (numerosDaSorte[5],"é menor que 50")
+}
